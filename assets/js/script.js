b037016faf071477350d66992c280492536b0fae
Lucas Dias Leandro <147958304+lucasserdev@users.noreply.github.com>
2024-04-07 15:06:58 -0300

    Initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,47 @@
+document.body.addEventListener('keyup', (e) => {
+    playSound(e.code.toLocaleLowerCase());
+})
+
+document.querySelector('button').addEventListener('click', () => {
+    let string = document.querySelector('#input').value;
+    
+    if (string !== '') {
+        let song = string.split('');
+        tocaString(song)
+    }
+})
+
+function playSound(sound) {
+    let key = document.querySelector(`audio#${sound}`);
+    let select = document.querySelector(`div#${sound}`);
+    if(key) {
+        key.currentTime = 0;
+        key.play();
+    }
+
+    select.classList.add('active');
+
+    setTimeout(() => {
+        select.classList.remove('active');
+    }, 300)
+}
+
+function tocaString(song) {
+    let wait = 0
+
+   for (let c of song) {
+    setTimeout( () => {
+        playSound(`key${c}`);
+    }, wait)
+    
+    wait += 250;
+   }
+
+}
+
+document.querySelector('.keys').addEventListener('click', (event) => {
+    let som = event.target.textContent;
+    let forma = (`key${som}`);
+    playSound(forma);
+})
+
